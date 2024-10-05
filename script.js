d6267e7761f7c0be775eaf7a984548c7cd4f1fad
sanika23 <sanikav7887@gmail.com>
2024-10-05 14:01:55 +0530

    new one

---
+++ script.js
@@ -0,0 +1,21 @@
+//converting celsius into fahrenheit
+
+let celsius=58;
+let fahrenheit=(celsius*9/5)+32;
+
+console.log("converted value of celsius is " ,fahrenheit);
+
+
+
+
+
+//positive negative number
+
+let a1=2;
+
+if(a1>=0){
+    console.log("number is positive");
+}
+else{
+    console.log("number is positive");
+}
